14c596ceb1b42156d841fe4bd54525ed64f9c81c
kapooraastha18 <99898793+kapooraastha18@users.noreply.github.com>
2025-04-06 03:30:14 +0530

    Update __init__.py

--- hummingbot/strategy/cm_williams_vix_maker/__init__.py
+++ hummingbot/strategy/cm_williams_vix_maker/__init__.py
@@ -1,1 +1,4 @@
 from .cm_williams_vix_maker import CMWilliamsVixMaker
+from .cm_williams_vix_maker_config_map import cm_williams_vix_maker_config_map
+
+__all__ = ["CMWilliamsVixMaker", "cm_williams_vix_maker_config_map"]
