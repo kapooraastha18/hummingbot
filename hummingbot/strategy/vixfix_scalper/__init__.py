dc998f5fd67385b0655dad63d665ca20bcfe3b9f
kapooraastha18 <99898793+kapooraastha18@users.noreply.github.com>
2025-04-06 15:49:36 +0530

    Create __init__.py

---
+++ hummingbot/strategy/vixfix_scalper/__init__.py
@@ -0,0 +1,4 @@
+from .vixfix_scalper import VixFixScalperStrategy
+from .vixfix_scalper_config_map import vixfix_scalper_config_map
+
+__all__ = ["VixFixScalperStrategy", "vixfix_scalper_config_map"]
